274613280cb79af70e57d6fe5dd37389e1fe2b38
Adrien Bonnin <adbonnin@users.noreply.github.com>
2022-09-03 14:39:28 +0200

    :sparkles: Initialisation du projet

---
+++ bin/deck.dart
@@ -0,0 +1,5 @@
+import 'package:deck/deck.dart' as deck;
+
+void main(List<String> arguments) {
+  print('Hello world: ${deck.calculate()}!');
+}
---
+++ test/deck_test.dart
@@ -0,0 +1,8 @@
+import 'package:deck/deck.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
